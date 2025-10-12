0a57c4b96099bed7f699de60f3c613e04ee16932
junipersharma1986-source <junipersharma1986@gmail.com>
2025-10-12 15:12:55 +0545

    Add files via upload

---
+++ operatorsPresidence.py
@@ -0,0 +1,15 @@
+v=4
+w=5
+x=8
+y=2
+z=0
+z=(v+w)*x/y;
+print("value of (v+w)*x/y is ", z)
+
+
+name="Aarohi"
+age=12
+if name=="Aarohi" or name=="John" and age >= 2 :
+    print("Hello! Welcome!")
+else:
+    print("good bye!")
