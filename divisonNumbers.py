24c5fe81fa4e1c16a891aa7e17f3476b324404a6
junipersharma1986-source <junipersharma1986@gmail.com>
2025-10-12 15:30:08 +0545

    Add files via upload

---
+++ divisonNumbers.py
@@ -0,0 +1,10 @@
+print ("enter number (numerator): ")
+numn=int(input())
+print ("enter number (numerator): ")
+numd=int(input())
+
+if numn%numd==0:
+     print ("\n" +str(numn)+ "is dvisible by" +str (numd))
+else:
+     print ("\n" +str(numn)+ "is not dvisible by" +str (numd))
+     3333
