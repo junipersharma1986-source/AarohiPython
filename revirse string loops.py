7aafbdc0bc896f3049bdedfa71001bebf0a1f005
junipersharma1986-source <junipersharma1986@gmail.com>
2025-10-19 15:14:52 +0545

    Add files via upload

---
+++ revirse string loops.py
@@ -0,0 +1,7 @@
+string=input("please inter your own string")
+string2=('')
+for i in string:
+    string2 = i + string2
+print("\n the orginal string = ", string)
+print("the reversed string = ", string2)
+
