db8e63e7d33e55f6770e3b95ca7c75d39b27bd2c
junipersharma1986-source <junipersharma1986@gmail.com>
2025-12-30 02:38:23 +0545

    Add files via upload

---
+++ countryCode.py
@@ -0,0 +1,19 @@
+SPCMcountry_code = {
+
+'India': '0091',
+
+'Australia': '0025',
+
+'Nepal': '00977'
+
+}
+
+print("country code for India -")
+
+print(SPCMcountry_code.get('India', "Not found"))
+
+print("country code for Japan -")
+
+print(SPCMcountry_code.get('Japan', "Not found"))
+                 
+
