9ecdaa99d35b641415913a52c3cc8c32ab51d239
junipersharma1986-source <junipersharma1986@gmail.com>
2026-01-03 14:23:52 +0545

    Add files via upload

--- guessssssssssssssssssssssssss.py
+++ guessssssssssssssssssssssssss.py
@@ -1,17 +1,17 @@
 import random
 print("welcome to guess the number")
-Secret_number = random.randint(1,100) 
+Secret_number = random.randint(1,80) 
 
 guess=None
 
 while guess != Secret_number:
-   guess = int(input("guess a number between 1 and 20"))
+   guess = int(input("guess a number between 1 and 80"))
 
    if guess< Secret_number:
       print("too low! try again!!")
    elif guess< Secret_number:
       print("too high! guess again!")
-   else:
+   else: 
       print("correct! you guessed the number!")
       
 
